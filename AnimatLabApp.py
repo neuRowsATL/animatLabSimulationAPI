6ca12af0b6e87dc0afad7d09b4be210c4daaaf8c
neuRowsATL <bchung4@student.gsu.edu>
2015-12-31 22:27:35 -0500

    Added main script to run: AnimatLabApp.py

---
+++ AnimatLabApp.py
@@ -0,0 +1,10 @@
+"""
+#AnimatLabApp.py
+
+Created by:      Bryce Chung
+Last modified:   December 31, 2015
+"""
+
+import class_animatLabModel as AnimatLabModel
+
+model = AnimatLabModel("F:/__DISSERTATION/SimulationFiles/_MASTER/FinalDissertationModel/")
