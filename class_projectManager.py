7acadafe9987f7a0bb74e2a60b10f8daf709ba99
neuRowsATL <bchung4@student.gsu.edu>
2016-01-15 10:43:22 -0500

    Initializing shell of ProjectManager class

--- class_projectManager.py
+++ class_projectManager.py
@@ -4,4 +4,57 @@ Last modified:   January 4, 2016
 """
 
 class ProjectManager(object):
-    pass
+    """
+    
+    """
+    
+    __init__(self, projName, obj_aproj=None, obj_simRunner=None, obj_simSet=None):
+        """
+        
+        """
+        
+        self.projName = projName
+        
+        self.aproj = obj_aproj
+        self.simRunner = obj_simRunner
+        self.simSet = obj_simSet
+        
+    
+    set_aproj(self, obj_aproj):
+        """
+        
+        """
+        
+        pass
+    
+    
+    set_simRunner(self, obj_simRunner):
+        """
+        
+        """
+        
+        pass
+    
+    
+    set_simSet(self, obj_simSet):
+        """
+        
+        """
+        
+        pass
+
+
+    make_asims(self):
+        """
+        
+        """
+        
+        pass
+    
+    
+    run(self):
+        """
+        
+        """
+        
+        pass
