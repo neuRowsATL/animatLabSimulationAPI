7bdae2be0c65c8640ba104d89633aecb0bcf15cd
neuRowsATL <bchung4@student.gsu.edu>
2016-01-15 11:01:53 -0500

    Added comments and some functionality to class

--- class_projectManager.py
+++ class_projectManager.py
@@ -8,43 +8,55 @@ class ProjectManager(object):
     
     """
     
-    __init__(self, projName, obj_aproj=None, obj_simRunner=None, obj_simSet=None):
+    __init__(self, projName, obj_aproj=None, obj_simRunner=None):
         """
+        __init__(projName, obj_aproj=None, obj_simRunner=None, obj_simSet=None)
+        Initiate ProjectManager object
         
+        projName        Unique name for ProjectManager object
+        obj_aproj       AnimatLabModel object
+        obj_simrunner   SimRunner object
+        
+        Last updated:   January 15, 2016
+        Modified by:    Bryce Chung
         """
         
         self.projName = projName
+        self.errorLog = {}
         
         self.aproj = obj_aproj
         self.simRunner = obj_simRunner
-        self.simSet = obj_simSet
         
     
     set_aproj(self, obj_aproj):
         """
+        set_aproj(obj_aproj)
+        Sets the AnimatLabModel object for the ProjectManager
         
+        obj_aproj       AnimatLabModel object for basis of simulations
+        
+        Last updated:   January 15, 2016
+        Modified by:    Bryce Chung
         """
         
-        pass
+        self.aproj = obj_aproj
     
     
     set_simRunner(self, obj_simRunner):
         """
+        set_simRunner(obj_simRunner)
+        Sets the simRunner object for the ProjectManager
         
+        obj_simRunner   SimulationRunner object for organizing simulations
+        
+        Last updated:   January 15, 2016
+        Modified by:    Bryce Chung
         """
         
-        pass
+        self.simRunner = obj_simRunner
     
-    
-    set_simSet(self, obj_simSet):
-        """
-        
-        """
-        
-        pass
 
-
-    make_asims(self):
+    make_asims(self, obj_simSet):
         """
         
         """
